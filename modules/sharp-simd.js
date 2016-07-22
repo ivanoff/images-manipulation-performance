d6b6a450b943a235e4aaffb7ae3f514d7213eafa
Lovell Fuller <github@lovell.info>
2016-07-22 15:02:41 +0100

    Add test case for sharp with SIMD feature enabled. Ensure sharp is required before canvas to avoid glib version clash. As sharp's default resize behaviour is to crop, remove the metadata/ extract step. This simplifyies code rather than improve performance.

--- modules/sharp-simd.js
+++ modules/sharp-simd.js
@@ -3,8 +3,8 @@
 var sharp = require('sharp');
 
 exports.process = function (fullImagePath, fullImagePathResult, size, callback) {
-  // Disable experimental SIMD support - this is the default behaviour
-  sharp.simd(false);
+  // Enable experimental SIMD support
+  sharp.simd(true);
 
   sharp(fullImagePath)
     .resize(size[0], size[1])
